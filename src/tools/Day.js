23db67f8f838242d29323992ec02be46cc10481d
Ed Lewis <ed.lewis@gmail.com>
2017-05-22 11:41:47 -0700

    improve combination of day styles

--- src/tools/Day.js
+++ src/tools/Day.js
@@ -10,26 +10,21 @@ class Day extends React.Component {
 
     render() {
       var dayOfWeek = this.props.day.format("dd"),
-          weekend = false,
-          firstWeek = false,
-          firstDay = false;
+        day = a.day,
+        weekend = null;
       if ( dayOfWeek === "Sa" || dayOfWeek === "Su" ) {
         weekend = a.weekend
       }
-      if ( this.props.day.date() < 8 && !this.props.firstWeek) { firstWeek = a.first_week; }
-      if ( this.props.day.date() === 1 && this.props.day.format("d") !== "0" ) { firstDay = a.first_day; }
+      if ( this.props.day.date() < 8 && !this.props.firstWeek) { day = a.first_week; }
+      if ( this.props.day.date() === 1 && this.props.day.format("d") !== "0" ) { day = a.first_day; }
       return (
-          <div className={ a.day }>
+          <div className={ day }>
             <div className={ weekend }>
-              <div className={ firstWeek }>
-                <div className={ firstDay }>
-                  <div className={a.date}>
-                      { this.props.day.format("D") }
-                  </div>
-                  <div className={a.textarea}>
-                      <textarea value={this.props.dayText} onChange={this.onChange.bind(this)}/>
-                  </div>
-                </div>
+              <div className={a.date}>
+                  { this.props.day.format("D") }
+              </div>
+              <div className={a.textarea}>
+                  <textarea value={this.props.dayText} onChange={this.onChange.bind(this)}/>
               </div>
             </div>
           </div>
